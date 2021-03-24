31df1708c9dbfe6dbdeddc816696994c1a435327
Vijay Lama <vijaylama2010@gmail.com>
2021-03-24 11:19:01 -0400

    Initial Commit

---
+++ Hacker Patrika/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hacker Patrika
+//
+//  Created by Vijay Lama on 3/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
